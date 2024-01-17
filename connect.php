d8c25c2420e3251c2dc820f1dbe42d457d3b7185
Serednii Mykola <seredniimykola@gmail.com>
2024-01-17 20:07:15 +0100

    add file connect.php

---
+++ connect.php
@@ -0,0 +1,6 @@
+<?php
+$servername = "mysql.zzz.com.ua";
+$username = "seredniimykola";
+$password = "tkfudimdcvFF55";
+$dbname = "seredniimykola";
+$connect = new mysqli($servername, $username, $password, $dbname);
